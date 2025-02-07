c605835f2f425de2c1bff4b91f2905cbf13710b4
Caleb Denio <cjdenio44@gmail.com>
2025-02-07 10:04:37 -0500

    Re-sync `boston.json` with GTFS feed

--- gtfs-importer/download-gtfs.sh
+++ gtfs-importer/download-gtfs.sh
@@ -1,4 +1,9 @@
 mkdir -p gtfs/
 
-wget https://cdn.mbta.com/MBTA_GTFS.zip                                   --max-redirect=0 -O gtfs/mbta.zip
-wget https://www.transitchicago.com/downloads/sch_data/google_transit.zip --max-redirect=0 -O gtfs/cta.zip
+function download {
+  wget $2 --accept="*.zip" -O gtfs/$1.zip
+}
+
+download mbta "https://cdn.mbta.com/MBTA_GTFS.zip"
+# download cta  "https://www.transitchicago.com/downloads/sch_data/google_transit.zip"
+# download bart "https://www.bart.gov/dev/schedules/google_transit.zip"
